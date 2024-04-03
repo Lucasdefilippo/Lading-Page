ae31557ed5bc8a4617810c9d3dbb28860edbba95
Lucas Defilippo <37743106+RolfNog@users.noreply.github.com>
2024-04-03 19:06:55 -0300

    OK!
    Landing Page está pronta, falta apenas colocar o @media para os devidos dispositivos.

--- assets/js/global/global.js
+++ assets/js/global/global.js
@@ -16,3 +16,29 @@
 
     desativarDragDrop();
 })();
+window.addEventListener("scroll", function () {
+    // Obtém a posição vertical atual da página
+    var posicaoPagina = window.scrollY;
+
+    // Obtém a posição da tag com a classe .um em relação ao topo da página
+    var posicaoUm = document.querySelector(".um").offsetTop;
+
+    // Se a posição atual da página for maior ou igual à posição da tag .um, inicia a animação da barra
+    if (posicaoPagina >= posicaoUm) {
+        // Adiciona a classe 'animado' à barra para iniciar a animação
+        document.querySelector(".um").classList.add("ativo");
+    }
+});
+window.addEventListener("scroll", function () {
+    // Obtém a posição vertical atual da página
+    var posicaoPagina = window.scrollY;
+
+    // Obtém a posição da tag com a classe .um em relação ao topo da página
+    var posicaoUm = document.querySelector(".tres").offsetTop;
+
+    // Se a posição atual da página for maior ou igual à posição da tag .um, inicia a animação da barra
+    if (posicaoPagina >= posicaoUm) {
+        // Adiciona a classe 'animado' à barra para iniciar a animação
+        document.querySelector(".tres").classList.add("ativo");
+    }
+});
